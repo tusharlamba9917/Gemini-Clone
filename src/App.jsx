040ff77dfc9de10e82f5e6505da978b764f9ddbd
Tushar Lamba <tushar.lamba@gspann.com>
2025-01-10 11:40:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar'
+import Main from './components/Main/main'
+import './index.css'
+import './App.css'
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  )
+}
+
+export default App
+
